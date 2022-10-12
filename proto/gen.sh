28aa1900cb993d920e1e707ad9120874f4130b85
Ekanant Phanuwatyingyong <osacn_ekanant.phanuwatyingyong@krungthai.com>
2022-10-12 16:00:38 +0700

    Change output polder from gen.sh

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,8 +1,9 @@
 work_dir=$(dirname "$0")
 cd ${work_dir}
+output_folder="grpc/generated"
 protoc -I . \
-    --go_out ../handler --go_opt paths=source_relative \
-    --go-grpc_out ../handler --go-grpc_opt paths=source_relative \
-    --grpc-gateway_out ../handler --grpc-gateway_opt paths=source_relative \
+    --go_out ../${output_folder} --go_opt paths=source_relative \
+    --go-grpc_out ../${output_folder} --go-grpc_opt paths=source_relative \
+    --grpc-gateway_out ../${output_folder} --grpc-gateway_opt paths=source_relative \
     *.proto
 echo "generate complete"
