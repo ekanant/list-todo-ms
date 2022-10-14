7e8339f1d8bf2862c10e32f9ef1b9568bda9be87
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2022-10-14 16:39:41 +0700

    Use https endpoint and divert to http or grpc

--- main.go
+++ main.go
@@ -4,57 +4,44 @@ import (
 	"context"
 	"list-todo-ms/grpc/generated"
 	"log"
-	"net"
 	"net/http"
+	"strings"
 
 	"list-todo-ms/handler"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	// Create a listener on TCP port
-	lis, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalln("Failed to listen:", err)
-	}
+	serverHandler := handler.NewHandler()
 
 	// Create a gRPC server object
-	s := grpc.NewServer()
+	grpcServer := grpc.NewServer()
 	// Attach the List Todo service to the server
-	generated.RegisterListToDoServer(s, handler.NewHandler())
-	// Serve gRPC server
-	log.Println("Serving gRPC on 0.0.0.0:8080")
-	go func() {
-		log.Fatalln(s.Serve(lis))
-	}()
-
-	// Create a client connection to the gRPC server we just started
-	// This is where the gRPC-Gateway proxies the requests
-	conn, err := grpc.DialContext(
-		context.Background(),
-		"0.0.0.0:8080",
-		grpc.WithBlock(),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		log.Fatalln("Failed to dial server:", err)
-	}
+	generated.RegisterListToDoServer(grpcServer, serverHandler)
 
 	gwmux := runtime.NewServeMux()
 	// Register List Todo
-	err = generated.RegisterListToDoHandler(context.Background(), gwmux, conn)
+	err := generated.RegisterListToDoHandlerServer(context.Background(), gwmux, serverHandler)
 	if err != nil {
 		log.Fatalln("Failed to register gateway:", err)
 	}
 
-	gwServer := &http.Server{
-		Addr:    ":8090",
-		Handler: gwmux,
+	log.Println("Serving gRPC-Gateway on https://0.0.0.0:8090")
+	err = http.ListenAndServeTLS(":8090", "./certs/server.pem", "./certs/server-key.pem", grpcHandlerFunc(*grpcServer, gwmux))
+	if err != nil {
+		log.Fatalln("Failed to start grpc", err)
 	}
+}
 
-	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
-	log.Fatalln(gwServer.ListenAndServe())
+func grpcHandlerFunc(grpcServer grpc.Server, otherHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.HasPrefix(
+			r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	})
 }
